c6e5ee4284ed2dada4377d1144703a9873401cf8
billysmartr <billysmartr@gmail.com>
2015-02-02 14:40:40 -0500

    added content to scrip to stand up node and ehterpad at the same time

--- etherpad_aws_bootstrap.sh
+++ etherpad_aws_bootstrap.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-#Line 2
+sudo yum upgrade -y
+sudo yum install gzip git curl python openssl-devel && sudo yum groupinstall "Development Tools"
+wget http://nodejs.org/dist/v0.10.36/node-v0.10.36.tar.gz
+tar -xzf node-v0.10.36.tar.gz 
+cd node-v0.10.36
+./configure
+make 
+sudo make install
+git clone https://github.com/ether/etherpad-lite.git
+cd etherpad-lite/
+bin/run.sh &
